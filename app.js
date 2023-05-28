24e7a7649e2b9921ea255f47359e6b50de31cc19
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-28 21:25:38 +0200

    Install express, body-parser, initialize server

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.get("/", (req, res) => {
+  const today = new Date();
+  console.log(today.getTime());
+
+  if (today.getDay() === 6 || today.getDay() === 0) {
+    res.send("Yay it is the weekend!.");
+  } else {
+    res.send("Fucking week.");
+  }
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+app.listen(3000, () => {
+  console.log("Server running on port 3000.");
 });
