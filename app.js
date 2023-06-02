01cdf3b9a7ffcb7c67d5184c161d0c5a1f1f0002
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-02 20:33:34 +0200

    Procfile and modified port to work on heroku

--- app.js
+++ app.js
@@ -237,6 +237,6 @@ app.get("/:listName", (req, res) => {
 app.get("/about", (req, res) => res.render("about"));
 
 // Get server to listen on port 3000
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000.");
 });
